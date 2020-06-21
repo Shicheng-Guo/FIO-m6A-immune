f516799485496737cd71cade65cd88da357cf305
mvpsc30 <42202293+mvpsc30@users.noreply.github.com>
2020-06-21 23:49:26 +0800

    Update 1.R

--- WGCNA/1.R
+++ WGCNA/1.R
@@ -11,22 +11,16 @@ enableWGCNAThreads()
 exprMat <- "新建文本文档.txt"
 
 
-# 官方推荐 "signed" 或 "signed hybrid"
-# 为与原文档一致，故未修改 
 type = "unsigned"
 
-# 相关性计算
-# 官方推荐 biweight mid-correlation & bicor
-# corType: pearson or bicor
-# 为与原文档一致，故未修改
+
 corType = "pearson"
 
 corFnc = ifelse(corType=="pearson", cor, bicor)
-# 对二元变量，如样本性状信息计算相关性时，
-# 或基因表达严重依赖于疾病状态时，需设置下面参数
+
 maxPOutliers = ifelse(corType=="pearson",1,0.05)
 
-# 关联样品性状的二元变量时，设置
+
 robustY = ifelse(corType=="pearson",T,F)
 
 ##导入数据##
